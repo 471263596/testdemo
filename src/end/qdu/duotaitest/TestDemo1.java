8411b7b41e3c97bbcd604de05bbcfa3086a95fb0
NIIT <NIIT@406-0153>
2016-05-07 13:44:58 +0800

    dogTest-exception switch engineer-information

--- src/end/qdu/duotaitest/TestDemo1.java
+++ src/end/qdu/duotaitest/TestDemo1.java
@@ -37,7 +37,24 @@ public class TestDemo1 {
          
      }
      public static int showMenu(){
-    	 
+    	 System.out.println("---------------------------------------");
+         System.out.println(":\t蓝桥计划Java工程师管理系统\t:");
+         System.out.println("---------------------------------------");
+         System.out.println("1.输入Java工程师资料");
+         System.out.println("2.删除指定Java工程师资料");
+         System.out.println("3.查询Java工程师资料");
+         System.out.println("4.修改Java工程师资料");
+         System.out.println("5.计算Java工程师的月薪");
+         System.out.println("6.保存新添加的Java工程师资料");
+         System.out.println("7.对Java工程师信息排序");
+         System.out.println("8.输出所有Java工程师信息");
+         System.out.println("9.清空所以Java工程师数据");
+         System.out.println("10.打印Java工程师数据报表");
+         System.out.println("11.从文件重新导入Java工程师数据");
+         System.out.println("0.结束（编辑工程师信息后提示保存）");
+         System.out.print("请输入您的选择：");
+         userSel=input.nextInt();
+         return userSel;
      }
      public static void ininputEngInfo(){
     	 
